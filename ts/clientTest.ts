b3c5c7affc984d63ac1af3cb0d811da7be269e01
wchang28 <wchang28@hotmail.com>
2016-05-17 17:41:41 -0400

    first comment

--- ts/clientTest.ts
+++ ts/clientTest.ts
@@ -4,10 +4,19 @@ import {MessageClient} from './MessageClient';
 let EventSource = require('eventsource');
 let $ = require('jquery-no-dom');
 
-let url = 'http://127.0.0.1:8080/proxy/events/event_stream';
+//let url = 'http://127.0.0.1:8080/proxy/events/event_stream';
 //let url = 'http://127.0.0.1:8080/api/events/event_stream';
+//let eventSourceInitDict = null;
 
-let msgBorker = new MsgBroker(() => new MessageClient(EventSource, $, url) , 10000);
+let url = 'https://harvesttesting.firstkeyholdings.com:47380/services/data/v35.0/events/event_stream';
+let eventSourceInitDict = {
+    headers: {
+        'Authorization': 'Bearer' + ' ' + 'Vi7HG3KiptvKF_MYKRSci1qgqNZ8SkF_bKL4RxgSYClN9VWy6nLrXay9lbZqBrjNluuLwRCgXOXRTTgTvUttoqTERGf2ZZbl'
+    }
+};
+
+
+let msgBorker = new MsgBroker(() => new MessageClient(EventSource, $, url, eventSourceInitDict) , 10000);
 
 msgBorker.on('connect', (conn_id:string) : void => {
     console.log('connected: conn_id=' + conn_id);
@@ -22,6 +31,7 @@ msgBorker.on('connect', (conn_id:string) : void => {
                     console.error('!!! Error: message send failed');
                 } else {
                     console.log('message sent successfully :-)');
+                    /*
                     console.log('unscribing the topic...');
                     msgBorker.unsubscribe(sub_id, (err:any):void => {
                         if (err) {
@@ -32,23 +42,12 @@ msgBorker.on('connect', (conn_id:string) : void => {
                             console.log('disconnected :-)'); 
                         }
                     });
+                    */
                 }
             });
         }
     });
 });
-
-/*
-msgBorker.on('connect', (conn_id:string) : void => {
-    console.log('connected: conn_id=' + conn_id);
-    console.log('sending a test message...');
-    msgBorker.send('topic/say_hi', {'location': 'USA'}, {'greeting':'good morning ' + new Date()}, (err: any) : void => {
-        console.log('test message sent disconnecting...');
-        msgBorker.disconnect();
-        console.log('disconnected');
-    });
-});
-*/
 
 msgBorker.on('client_open', (): void => {
     console.log('client_open');
