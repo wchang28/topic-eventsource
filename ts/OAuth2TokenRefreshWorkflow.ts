83169ebe0fe505774dfcfad1171e138ba639735c
wchang28 <wchang28@hotmail.com>
2016-05-19 07:26:15 -0400

    cleanup code

--- ts/OAuth2TokenRefreshWorkflow.ts
+++ ts/OAuth2TokenRefreshWorkflow.ts
@@ -1,6 +1,18 @@
 /// <reference path="../typings/node/node.d.ts" />
 import * as events from 'events';
 
+interface ICompletionHandler {
+    (err: any, ret: any) : void;
+}
+
+interface IAjaxon {
+    (method: string, url: string, data:any, done: ICompletionHandler, headers?: any, rejectUnauthorized?:boolean) : void;
+}
+
+interface IEventSourceFactory {
+    (url: string, eventSourceInitDic: any, done: ICompletionHandler) : void
+}
+
 interface IOAuth2Access {
     instance_url: string;
     token_type: string;
@@ -13,38 +25,34 @@ interface IOAuth2TokenRefresher {
     refreshAccessToken: (refresh_token: string, done: (err: any, newAccess: IOAuth2Access) => void) => void;
 }
 
-interface IActioCompletionHandler {
-    (err: any, data: any) : void;
+interface IWorkflowCall {
+    call : (url: string, headers:any, done: ICompletionHandler) => void;
 }
 
-interface IWorkflowAction {
-    execute : (url: string, headers:any, done: IActioCompletionHandler) => void;
-}
-
-class AJaxonCall implements IWorkflowAction {
-    constructor(protected $J: any, protected method: string, protected data: any, protected rejectUnauthorized?:boolean) {}
-    execute(url: string, headers:any, done: IActioCompletionHandler) : void {
+class AJaxonCall implements IWorkflowCall {
+    constructor(protected $J: IAjaxon, protected method: string, protected data: any, protected rejectUnauthorized?:boolean) {}
+    call(url: string, headers:any, done: ICompletionHandler) : void {
         this.$J(this.method, url, this.data, done, headers, this.rejectUnauthorized);
     }
 }
 
-class EventSourceCall implements IWorkflowAction {
-    constructor(protected $E: any, protected rejectUnauthorized?:boolean) {}
-    execute(url: string, headers:any, done: IActioCompletionHandler) : void {
+class EventSourceCall implements IWorkflowCall {
+    constructor(protected $E: IEventSourceFactory, protected rejectUnauthorized?:boolean) {}
+    call(url: string, headers:any, done: ICompletionHandler) : void {
         let eventSourceInitDic = {headers: headers, rejectUnauthorized: this.rejectUnauthorized};
         this.$E(url, eventSourceInitDic, done);
     }
 }
 
 export class OAuth2TokenRefreshWorkflow extends events.EventEmitter {
-    constructor(protected $J_: any, protected $E_: any, protected access: IOAuth2Access, protected tokenRefresher: IOAuth2TokenRefresher) {
+    constructor(protected $J_: IAjaxon, protected $E_: IEventSourceFactory, protected access: IOAuth2Access, protected tokenRefresher: IOAuth2TokenRefresher) {
         super();
     }
     
     private getAuthorizedHeaders(access: IOAuth2Access) : any {return {'Authorization' : access.token_type + " " + access.access_token};}
     
-    private executeTokenRefreshWorkflow(workFlowAction: IWorkflowAction, path: string, done: IActioCompletionHandler) {
-        workFlowAction.execute(this.access.instance_url+path, this.getAuthorizedHeaders(this.access), (err: any, ret: any) => {
+    private executehWorkflow(workFlowCall: IWorkflowCall, pathname: string, done: ICompletionHandler) {
+        workFlowCall.call(this.access.instance_url+pathname, this.getAuthorizedHeaders(this.access), (err: any, ret: any) => {
              if (err) {
                 if (this.tokenRefresher.isTokenExpiredError(err) && this.access.refresh_token) {
                     this.tokenRefresher.refreshAccessToken(this.access.refresh_token, (err: any, newAccess: IOAuth2Access) : void => {
@@ -52,7 +60,7 @@ export class OAuth2TokenRefreshWorkflow extends events.EventEmitter {
                             done(err, null);
                         else {
                             this.emit('on_access_refreshed', newAccess);
-                            workFlowAction.execute(newAccess.instance_url + path, this.getAuthorizedHeaders(newAccess), done);
+                            workFlowCall.call(newAccess.instance_url + pathname, this.getAuthorizedHeaders(newAccess), done);
                         }
                     });
                 } else
@@ -62,13 +70,15 @@ export class OAuth2TokenRefreshWorkflow extends events.EventEmitter {
         });
     }
     
-    $J(method: string, path:string, data:any, done: IActioCompletionHandler, rejectUnauthorized?: boolean) : void {
+    // workflow's $J method
+    $J(method: string, pathname:string, data:any, done: ICompletionHandler, rejectUnauthorized?: boolean) : void {
         let action = new AJaxonCall(this.$J_, method, data, rejectUnauthorized);
-        this.executeTokenRefreshWorkflow(action, path, done);
+        this.executehWorkflow(action, pathname, done);
     }
     
-    $E(path: string, done: IActioCompletionHandler, rejectUnauthorized?: boolean) : void {
+    // workflow's $E method
+    $E(pathname: string, done: ICompletionHandler, rejectUnauthorized?: boolean) : void {
         let action = new EventSourceCall(this.$E_, rejectUnauthorized);
-        this.executeTokenRefreshWorkflow(action, path, done);
+        this.executehWorkflow(action, pathname, done);
     }
 }
