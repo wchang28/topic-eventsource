094c6a79c35a4e424370dc6fcdfb60d24f7d3585
wchang28 <wchang28@hotmail.com>
2016-08-15 09:34:19 -0400

    ytheyhety

--- ts/$-driver.ts
+++ ts/$-driver.ts
@@ -1,4 +1,4 @@
-import {IError, ApiCallOptions} from 'rest-api-interfaces';
+import * as restIntf from 'rest-api-interfaces';
 import * as eventSource from 'eventsource-typings';
 
 export interface IEventSourceConnectCompletionHandler {
@@ -6,7 +6,7 @@ export interface IEventSourceConnectCompletionHandler {
 }
 
 export interface $Driver {
-    $J: (method:string, url:string, data:any, done:(err:IError, ret:any) => void, options?: ApiCallOptions) => void;
-    $E: (url:string, done: IEventSourceConnectCompletionHandler, options?: ApiCallOptions) => void;
-    $F: (url:string, formData:any, done:(err:IError, ret:any) => void, options?: ApiCallOptions) => void;
+    $J: (method:string, url:string, data:any, done:(err:restIntf.IError, ret:any) => void, options?: restIntf.ApiCallOptions) => void;
+    $E: (url:string, done: IEventSourceConnectCompletionHandler, options?: restIntf.ApiCallOptions) => void;
+    $F: (url:string, formData:any, done:(err:restIntf.IError, ret:any) => void, options?: restIntf.ApiCallOptions) => void;
 }
