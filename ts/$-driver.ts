07f2518fde29ebcf870c4898ed9c81e55182504e
wchang28 <wchang28@hotmail.com>
2016-08-15 09:20:44 -0400

    ewferf

--- ts/$-driver.ts
+++ ts/$-driver.ts
@@ -1,8 +1,12 @@
 import {IError, ApiCallOptions} from 'rest-api-interfaces';
 import * as eventSource from 'eventsource-typings';
 
+export interface IEventSourceConnectCompletionHandler {
+    (err: eventSource.Error, eventSource: eventSource.IEventSource) : void;
+}
+
 export interface $Driver {
     $J: (method:string, url:string, data:any, done:(err:IError, ret:any) => void, options?: ApiCallOptions) => void;
-    $E: (url:string, done:(err: eventSource.Error, eventSource: eventSource.IEventSource) => void, options?:ApiCallOptions) => void;
+    $E: (url:string, done: IEventSourceConnectCompletionHandler, options?: ApiCallOptions) => void;
     $F: (url:string, formData:any, done:(err:IError, ret:any) => void, options?: ApiCallOptions) => void;
 }
