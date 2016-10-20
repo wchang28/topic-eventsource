1d4182b001e1509297bfc59a939b3ae975216365
wchang28 <wchang28@hotmail.com>
2016-10-20 19:02:08 -0400

    dghnhdgndg

--- ts/api/index.ts
+++ ts/api/index.ts
@@ -4,8 +4,30 @@ import * as tr from 'rcf-message-router';
 
 let router = express.Router();
 
+let topicAuthApp = express();
+
+topicAuthApp.use('/topic/say_hi', (req:express.Request, res:express.Response, next: express.NextFunction) => {
+    let {authReq, authRes} = tr.getDestinationAuthReqRes(req, res);
+    console.log('USE /topic/say_hi:' + JSON.stringify(authReq, null, 2));
+    next();
+});
+
+topicAuthApp.get('/topic/say_hi', (req:express.Request, res:express.Response, next: express.NextFunction) => {
+    let {authReq, authRes} = tr.getDestinationAuthReqRes(req, res);
+    console.log('<GET/SUBSCRIBE>: /topic/say_hi');
+    authRes.accept();
+});
+
+topicAuthApp.post('/topic/say_hi', (req:express.Request, res:express.Response, next: express.NextFunction) => {
+    let {authReq, authRes} = tr.getDestinationAuthReqRes(req, res);
+    console.log('<POST/SEND>: /topic/say_hi');
+    //authRes.reject("not authorized :-(");
+    authRes.accept();
+});
+
 let trOptions: tr.Options = {
     pingIntervalMS: 5000
+    ,destinationAuthorizeApp: topicAuthApp
 };
 
 let topicRouter = tr.getRouter('/event_stream', trOptions);
@@ -20,11 +42,11 @@ topicRouter.connectionsManager.on('change', () => {
     console.log("");
 });
 topicRouter.eventEmitter.on('client_connect', (params: tr.ConnectedEventParams) : void => {
-    console.log('clinet ' + params.conn_id + ' @ ' + params.remoteAddress + ' connected to the SSE topic endpoint');
-});
-
-topicRouter.eventEmitter.on('client_disconnect', (params: tr.ConnectedEventParams) : void => {
-    console.log('clinet ' + params.conn_id + ' @ ' + params.remoteAddress +  ' disconnected from the SSE topic endpoint');
+    console.log('clinet ' + params.conn_id + ' @ ' + params.remoteAddress + ' connected to the SSE topic endpoint, no. conn = ' + topicRouter.connectionsManager.ConnectionsCount);
+}).on('client_disconnect', (params: tr.ConnectedEventParams) : void => {
+    console.log('clinet ' + params.conn_id + ' @ ' + params.remoteAddress +  ' disconnected from the SSE topic endpoint, no. conn = ' + topicRouter.connectionsManager.ConnectionsCount);
+}).on('on_client_send_msg', (params: tr.ClientSendMsgEventParams) => {
+    console.log('\nclinet just sent the following message:\n' + JSON.stringify(params, null, 2));
 });
 
 import * as sobject from './sobject';
