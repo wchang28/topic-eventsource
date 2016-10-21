fca1c84e8b3710a1749d35766823f598c72c8818
wchang28 <wchang28@hotmail.com>
2016-10-21 13:06:38 -0400

    trwgwrtg

--- ts/api/index.ts
+++ ts/api/index.ts
@@ -35,12 +35,14 @@ let topicRouter = tr.getRouter('/event_stream', trOptions);
 router.use('/events', topicRouter); // topic subscription endpoint is available at /events/event_stream from this route
 
 topicRouter.connectionsManager.on('change', () => {
+    /*
     console.log("");
     console.log("api topic router's connectionsManager changed");
     console.log("======================================================");
     console.log(JSON.stringify(topicRouter.connectionsManager));
     console.log("======================================================");
     console.log("");
+    */
 });
 topicRouter.eventEmitter.on('client_connect', (params: tr.ConnectedEventParams) : void => {
     console.log('clinet ' + params.conn_id + ' @ ' + params.remoteAddress + ' connected to the SSE topic endpoint, no. conn = ' + topicRouter.connectionsManager.ConnectionsCount);
